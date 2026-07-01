eeac6a457f267ed75513b7244f7e59b116558606
shreyash <shreyashmandal404>
2026-07-01 18:20:51 +0530

    Add ASCII code programs

---
+++ ASCII Code/program_no1.c
@@ -0,0 +1,13 @@
+//Q2 Find the ASCII code from character.
+#include<stdio.h>
+int main()
+{
+    char small='a';
+    char capital='A';
+
+    printf("\nThe ASCII code of character '%c' is '%d',small, small");
+
+    printf("\nThe ASCII code of character '%c' is '%d',capital, capital");
+    return 0;
+}
+
---
+++ ASCII Code/program_no2.c
@@ -0,0 +1,9 @@
+// Q2 Find the Character code from ASCII.
+#include<stdio.h>
+int main()
+{
+    int number=35;
+
+    printf("\nThe character of ASCII code is: %c",number);
+    return 0;
+}

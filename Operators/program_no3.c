7234828b44a926c475b58860a33afb663c6f42e5
shreyash <shreyashmandal404>
2026-07-08 18:51:37 +0530

    Arithmatic Operators

---
+++ Operators/program_no3.c
@@ -0,0 +1,21 @@
+//   * Arithmatic Operator *  //
+#include<stdio.h>
+int main()
+{
+
+    int num1,num2;
+
+    printf("\n Enter the Number 1:");
+    scanf("%d",&num1);
+
+    printf("\n Enter the Number 2:");
+    scanf("%d",&num2);
+
+    printf("\n Addition is: %d",num1 + num2);
+    printf("\n Substraction is: %d",num1 - num2);
+    printf("\n Multiplication is: %d",num1 * num2);
+    printf("\n Division is: %d",num1 / num2);
+    printf("\n Module is: %d",num1 % num2);
+
+    return 0;
+}

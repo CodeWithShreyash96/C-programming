60c2c88257c578a167135ea283a3ac5d508d0936
shreyash <shreyashmandal404>
2026-07-09 15:36:40 +0530

    Increment/Decrement Operators

---
+++ Operators/program_no4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+
+    printf("\n Enter the Number a:");
+    scanf("%d",&a);
+
+    printf("\n Enter the Number b:");
+    scanf("%d",&b);
+
+    printf("\n the increment is : %d", ++a);
+
+    printf("\n the decrement is : %d", --a);
+
+    printf("\n the increment is : %d", ++b);
+
+    printf("\n the decrement is : %d", --b);
+
+    return  0;
+}

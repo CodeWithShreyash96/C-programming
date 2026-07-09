9aa840d724aa06f8500f219d572552fdafc370d6
shreyash <shreyashmandal404>
2026-07-09 15:48:42 +0530

    Assignment Operators

---
+++ Operators/program_no5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+
+    printf("\n Enter the number a:");
+    scanf("%d",&a);
+
+    printf("\n Enter the number b");
+    scanf("%d",&b);    
+
+    printf("\n a=b  = %d", a=b);
+    printf("\n a+=b = %d", a+=b);
+    printf("\n a-=b = %d", a-=b);
+    printf("\n a*=b = %d", a*=b);
+    printf("\n a/=b = %d", a/=b);
+
+    return 0;
+}

ce9a5a4fa15f0dd2a87d68c79c86e14fecfb7f2f
shreyash <shreyashmandal404>
2026-07-16 12:22:23 +0530

    Check the Number is positive, Negative or Zero

---
+++ Control_Statements/program_no4.c
@@ -0,0 +1,23 @@
+// if-else program //
+#include<stdio.h>
+int main()
+{
+    int num;
+
+    printf("Enter the Number:");
+    scanf("%d",&num);
+
+    if(num>0)
+    {
+        printf("The Number is positive");0
+    }
+    else if(num<0)
+    {
+        printf("The Number is negative");
+    }
+    else
+    {
+        printf("The Number is zeo");
+    }
+    return 0;
+}

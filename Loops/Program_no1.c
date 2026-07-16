433d7a5353246a0fd8c3d7b5512fd69f7693ec98
shreyash <shreyashmandal404>
2026-07-16 12:31:52 +0530

    print the Number 1 to 100 using For loop

---
+++ Loops/Program_no1.c
@@ -0,0 +1,13 @@
+// For loop //
+// print the number 1 to 100 //
+#include<stdio.h>
+int main()
+{
+    int num;
+
+    for(num=1 ; num<=100 ; num++)
+    {
+        printf("\n %d",num);
+    }
+    return 0;
+}

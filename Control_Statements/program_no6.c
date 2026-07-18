80b7dbd65b3da198ed2a6b9c2ee6c07110a364a6
shreyash <shreyashmandal404@gmail.com>
2026-07-18 16:58:58 +0530

    Check the Character is Vowel or Consanant

---
+++ Control_Statements/program_no6.c
@@ -0,0 +1,21 @@
+// if-else statement //
+// check vowel or consanant //
+#include<stdio.h>
+int main()
+{
+    char ch;
+
+    printf("Enter the character:");
+    scanf("%c",&ch);
+
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ||
+        ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+        {
+            printf("The character %c is vowel");
+        }
+    else
+    {
+            printf("The character %c is consanant");
+    }
+    return 0;
+}
